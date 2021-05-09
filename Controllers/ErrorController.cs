4c678cf50de1e19ea68dc6ee8256312cf31611a4
firepacket <3860274+firepacket@users.noreply.github.com>
2021-05-09 18:01:14 -0400

    Added custom errors and enhanced security of web.config
    Added the views and controllers for the new errors. Added the new errors in web.config along with other important security headers (turned off debug flag) and removed some unused modules

--- Controllers/ErrorController.cs
+++ Controllers/ErrorController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AnarkRE.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,11 +14,64 @@ namespace AnarkRE.Controllers
             ViewBag.Title = "Error!";
             return View();
         }
+        public ActionResult Expired()
+        {
+            ViewBag.Tite = "Page expired";
+            
+            return View();
+        }
+        public ActionResult Banned()
+        {
+            ViewBag.Tite = "Banned";
+            return View();
+        }
 
         public ActionResult Notfound()
         {
             ViewBag.Title = "404 Not Found";
+            Response.StatusCode = 404;
+            Response.TrySkipIisCustomErrors = true;
             return View();
         }
+        public ActionResult Notallowed()
+        {
+            ViewBag.Title = "Action not allowed";
+            Response.StatusCode = 405;
+            Response.TrySkipIisCustomErrors = true;
+            return View();
+        }
+
+        public ActionResult Unauthorized()
+        {
+            Response.StatusCode = 401;
+            Response.TrySkipIisCustomErrors = true;
+            ViewBag.Title = "Unauthorized";
+            return View();
+        }
+
+        public ActionResult Aggressive()
+        {
+            Response.StatusCode = 429;
+            Response.TrySkipIisCustomErrors = true;
+            ViewBag.Title = "Aggressive";
+            return View();
+        }
+
+        public ActionResult BadRequest()
+        {
+            Response.StatusCode = 400;
+            Response.TrySkipIisCustomErrors = true;
+            ViewBag.Tite = "Bad Request";
+            return View();
+        }
+
+        public ActionResult Forbidden()
+        {
+            Response.StatusCode = 403;
+            Response.TrySkipIisCustomErrors = true;
+            ViewBag.Title = "Forbidden";
+            return View();
+        }
+
     }
 }
